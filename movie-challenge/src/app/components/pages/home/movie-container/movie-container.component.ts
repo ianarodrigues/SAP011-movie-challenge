cbc23d115eb15d20488a3bdfeb53c83c1f28a60a
ianarodrigues <moniquesrlima@gmail.com>
2023-11-10 14:34:38 -0300

    T005: feat-movie-container component implementation

---
+++ movie-challenge/src/app/components/pages/home/movie-container/movie-container.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { TmdbService } from 'src/app/services/tmdb/tmdb.service';
+
+@Component({
+  selector: 'app-movie-container',
+  templateUrl: './movie-container.component.html',
+  styleUrls: ['./movie-container.component.css']
+})
+export class MovieContainerComponent {
+  @Input() movies: any[] = [];
+
+  constructor() {
+  }
+
+
+}
