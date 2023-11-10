a1b81ed103341b197274ffce019715d419c9b9cf
ianarodrigues <moniquesrlima@gmail.com>
2023-11-10 11:07:10 -0300

    T003: feat-menu component implementation

---
+++ movie-challenge/src/app/components/commons/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TmdbService } from 'src/app/services/tmdb/tmdb.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+}
