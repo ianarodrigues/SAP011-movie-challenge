7a87cafc5b26412945a4dd618459e4ed68e7d340
ianarodrigues <moniquesrlima@gmail.com>
2023-11-24 17:44:21 -0300

    T008: feat-movie-details implementation of on-screen animation and route correction

--- movie-challenge/src/app/app.module.ts
+++ movie-challenge/src/app/app.module.ts
@@ -1,30 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/commons/menu/menu.component';
 import { FooterComponent } from './components/commons/footer/footer.component';
-import { HomeModule } from './components/pages/home/home.module';
 import { MovieDetailsModule } from './components/pages/movie-details/movie-details.module';
 import { TmdbService } from './services/tmdb/tmdb.service';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    FooterComponent, 
-  ],
+  declarations: [AppComponent, MenuComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    HttpClientModule, 
-    MovieDetailsModule
+    AppRoutingModule,
+    HttpClientModule,
+    MovieDetailsModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    FormsModule, 
   ],
-  providers: [
-    TmdbService
-  ],
-  bootstrap: [AppComponent]
+  providers: [TmdbService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
